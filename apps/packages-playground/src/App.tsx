0d3ce5a56ade28cc1a56e8e389b9de56eebbdb60
ahmadshaheer <ashaheerki@gmail.com>
2024-09-22 19:20:46 +0430

    test design tokens in packages playground

--- apps/packages-playground/src/App.tsx
+++ apps/packages-playground/src/App.tsx
@@ -1,18 +1,31 @@
 import "./App.css";
 import { ThemeSwitcher, useTheme } from "@shaheerkochai/core";
 import { Button } from "@shaheerkochai/button";
+import { Color, Typography } from "@shaheerkochai/design-tokens";
+import { Spacing } from "@shaheerkochai/design-tokens";
 
 function App() {
   const { theme } = useTheme();
 
   return (
     <>
-      <h1>My Themed App</h1>
+      <h1>Theme Switcher and Button</h1>
       <ThemeSwitcher />
 
       <Button theme={theme} variant="secondary">
         hey
       </Button>
+
+      <h1>Design tokens</h1>
+      <p
+        style={{
+          background: Color.BG_AMBER_400,
+          letterSpacing: Spacing.MARGIN_10,
+          fontSize: Typography.FONTSIZE_3XL,
+        }}
+      >
+        hey
+      </p>
     </>
   );
 }
--- apps/packages-playground/src/main.tsx
+++ apps/packages-playground/src/main.tsx
@@ -2,12 +2,22 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ThemeProvider } from "@shaheerkochai/core";
+import { ThemeProvider, useTheme } from "@shaheerkochai/core";
+
+function ThemedApp() {
+  const { theme } = useTheme();
+
+  return (
+    <div className={`app ${theme}`}>
+      <App />
+    </div>
+  );
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <ThemedApp />
     </ThemeProvider>
   </StrictMode>
 );
