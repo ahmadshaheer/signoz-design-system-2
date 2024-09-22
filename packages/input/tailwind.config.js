289399a7d3618f94a2ac08c67ca02f4869816087
ahmadshaheer <ashaheerki@gmail.com>
2024-09-22 12:24:53 +0430

    remove input

--- packages/input/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-const sharedConfig = require("@shaheerkochai/ui-config/tailwind.config.js");
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  ...sharedConfig,
-  content: [...sharedConfig.content, "./src/**/*.{js,ts,jsx,tsx}"],
-};
